b66a9c1142ba2dd37b276e258c87ff71e7e0c439
TAKUMI HONDA <TAKUMI.HONDA@grp.tmnf.jp>
2025-10-06 14:25:33 +0900

    テストページ実装

---
+++ src/pages/before.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goToAfter = () => router.push("/after");
+
+const onScroll = () => {
+  alert("scroll");
+};
+onMounted(() => window.addEventListener("scroll", onScroll));
+</script>
+
+<template>
+  <div>スクロールのイベントをリスナーを登録した（スクロールしてみてください）</div>
+  <button @click="goToAfter">次へ</button>
+  <div>あ</div>
+  <div>い</div>
+  <div>う</div>
+  <div>え</div>
+  <div>お</div>
+  <div>か</div>
+  <div>き</div>
+  <div>く</div>
+  <div>け</div>
+  <div>こ</div>
+  <div>さ</div>
+  <div>し</div>
+  <div>す</div>
+  <div>せ</div>
+  <div>そ</div>
+  <div>た</div>
+  <div>ち</div>
+  <div>つ</div>
+  <div>て</div>
+  <div>と</div>
+  <div>な</div>
+  <div>に</div>
+  <div>ぬ</div>
+  <div>ね</div>
+  <div>の</div>
+  <div>は</div>
+  <div>ひ</div>
+  <div>ふ</div>
+  <div>へ</div>
+  <div>ほ</div>
+  <div>ま</div>
+  <div>み</div>
+  <div>む</div>
+  <div>め</div>
+  <div>も</div>
+  <div>や</div>
+  <div>ゆ</div>
+  <div>よ</div>
+  <div>ら</div>
+  <div>り</div>
+  <div>る</div>
+  <div>れ</div>
+  <div>ろ</div>
+  <div>わ</div>
+  <div>を</div>
+  <div>ん</div>
+  <div>あ</div>
+  <div>い</div>
+  <div>う</div>
+  <div>え</div>
+  <div>お</div>
+  <div>か</div>
+  <div>き</div>
+  <div>く</div>
+  <div>け</div>
+  <div>こ</div>
+  <div>さ</div>
+  <div>し</div>
+  <div>す</div>
+  <div>せ</div>
+  <div>そ</div>
+  <div>た</div>
+  <div>ち</div>
+  <div>つ</div>
+  <div>て</div>
+  <div>と</div>
+  <div>な</div>
+  <div>に</div>
+  <div>ぬ</div>
+  <div>ね</div>
+  <div>の</div>
+  <div>は</div>
+  <div>ひ</div>
+  <div>ふ</div>
+  <div>へ</div>
+  <div>ほ</div>
+  <div>ま</div>
+  <div>み</div>
+  <div>む</div>
+  <div>め</div>
+  <div>も</div>
+  <div>や</div>
+  <div>ゆ</div>
+  <div>よ</div>
+  <div>ら</div>
+  <div>り</div>
+  <div>る</div>
+  <div>れ</div>
+  <div>ろ</div>
+  <div>わ</div>
+  <div>を</div>
+  <div>ん</div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {path: '/', component: () => import('../pages/before.vue')},
+    {path: '/after', component: () => import('../pages/after.vue')},
+  ],
 })
 
 export default router
